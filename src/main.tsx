77ad21b210f02ef929f44e1783f7f1bdc2a411d9
kenarigame <kenarigame@gmail.com>
2026-06-29 12:42:28 +0700

    save project

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import ContactSection from './components/sections/Contact';
-import './index.css';
-import AboutUs from './pages/AboutUs';
-import BlogDetail from './pages/BlogDetail';
-import CreateBlog from './pages/CreateBlog';
-import Homepage from './pages/Homepage';
-import Login from './pages/Login';
-import Services from './pages/Service';
-
-
+import ContactSection from "./components/sections/Contact";
+import "./index.css";
+import AboutUs from "./pages/AboutUs";
+import BlogDetail from "./pages/BlogDetail";
+import CreateBlog from "./pages/CreateBlog";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Services from "./pages/Service";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
   },
-   {
+  {
     path: "/about",
     element: <AboutUs />,
   },
@@ -34,10 +32,6 @@ const router = createBrowserRouter([
     path: "/createblog",
     element: <CreateBlog />,
   },
-  // {
-  //   path: "/teams",
-  //   element: <TeamCard />,
-  // },
   {
     path: "/login",
     element: <Login />,
@@ -48,8 +42,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
